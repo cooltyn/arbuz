eda017ff2db3300b54286f079e5f0ac6de8f37d9
cooltyn <cooltyn@mail.ru>
2018-07-21 02:13:01 +0300

    3

--- static/js/index.js
+++ static/js/index.js
@@ -10,9 +10,9 @@ window.addEventListener('load', function() {
 			result: function() {
 				var mass = this.mass/1000;
 				var length_e = this.length_e/100;
+				var length_m = this.length_m/100;
 				var r_e = length_e/(2*Math.PI);
-				var length_m = Math.max(4*r_e, this.length_m/100);
-				var r_m = Math.sqrt(Math.abs(Math.pow(length_m,2)/(2*Math.pow(Math.PI,2))-Math.pow(r_e,2)));
+				var r_m = Math.max(0, Math.sqrt(Math.pow(length_m,2)/(2*Math.pow(Math.PI,2))-Math.pow(r_e,2)));
 				var v = Math.PI*Math.pow(r_e,2)*r_m*4/3;
 				console.log(mass, length_e, length_m, r_e, r_m, v);
 				return (mass/v).toFixed(3) || null;
